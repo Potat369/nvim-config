635e05fd9e722416f51677ccc9a19dd0e19aa8eb
Potat369 <yevheniidemian@gmail.com>
2025-02-24 21:11:33 +0200

    Add telescope.nvim plugin

---
+++ lua/plugins/fuzzyfinder.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	init = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>fc", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end)
+		vim.keymap.set("n", "<leader>ff", builtin.find_files)
+	end,
+}
