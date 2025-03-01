c277bc65ff1d226d73c42bc4cfd099e482401ff5
Potat369 <yevheniidemian@gmail.com>
2025-03-01 10:56:29 +0200

    Setup lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,21 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
+	opts = {
+		servers = {
+			lua_ls = {
+				server_capabilities = {
+					semanticTokensProvider = vim.NIL,
+				},
+			},
+			pylsp = {},
+		},
+	},
+	config = function(_, opts)
+		local lspconfig = require("lspconfig")
+		for server, config in pairs(opts.servers) do
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			lspconfig[server].setup(config)
+		end
+	end,
+}
