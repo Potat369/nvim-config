ca101e565b0ba4142bfe124ef2f1e104987666fb
Potat369 <yevheniidemian@gmail.com>
2025-03-01 10:56:09 +0200

    Add completion plugin

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,21 @@
+return {
+	"saghen/blink.cmp",
+	version = "*",
+	opts = {
+		signature = { enabled = true },
+		sources = {
+			providers = {
+				cmdline = {
+					enabled = function()
+						return vim.fn.getcmdtype() ~= ":" or not vim.fn.getcmdline():match("^[%%0-9,'<>%-]*!")
+					end,
+				},
+			},
+		},
+		fuzzy = {
+			prebuilt_binaries = {
+				force_version = "v0.13.0",
+			},
+		},
+	},
+}
